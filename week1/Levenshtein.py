12a523c84234564176197c7dc5277fa1a3a88dfa
Jim <jim@jim-Aspire-M3100.(none)>
2012-03-15 19:55:09 +0100

    test

--- week1/Levenshtein.py
+++ week1/Levenshtein.py
@@ -5,7 +5,8 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
-        for f in sys.argv[1:]:
-            print f
+        for word in sys.argv[1:]:
+            print word
+            
     else:
         print "usage: Levenshtein <word one> <word two>"
